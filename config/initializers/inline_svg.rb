1593d57835b0720986ce5410d7e2094ba5e3c17a
James Martin <jamesmartin@github.com>
2017-04-10 17:42:32 +1000

    Demo of custom asset finder. Noop

--- config/initializers/inline_svg.rb
+++ config/initializers/inline_svg.rb
@@ -1,6 +1,12 @@
 # Add a custom transformation
 
+class AssetFinder
+  def find_asset(name)
+  end
+end
+
 InlineSvg.configure do |config|
+  #config.asset_finder = AssetFinder.new
   config.add_custom_transformation(attribute: :custom, transform: MyCustomTransform)
   config.add_custom_transformation(attribute: :default, transform: DefaultTransform, default_value: 'some-default')
 end
