bea429289ddcfa3afe7f4f1c8d6218fdff1d9081
James Martin <jamesmartin@github.com>
2019-06-18 17:04:03 +1000

    Remove active_record so we don't need a database

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,20 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+#require 'rails/all'
+# active_record is what we're not going to use it, so comment it "just in case"
+# require "active_record/railtie"
+
+# This is not loaded in rails/all but inside active_record so add it if
+# you want your models work as expected
+require "active_model/railtie"
+# And now the rest
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine" # Only for Rails >= 5.0
+require "active_storage/engine" # Only for Rails >= 5.2
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
